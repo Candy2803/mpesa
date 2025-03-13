cac4638de2edfd1fc92be233ec26ab6667c757fd
+ <+>
2025-03-13 13:59:41 +0300

    First commit

--- config/mpesa.js
+++ config/mpesa.js
@@ -5,10 +5,11 @@ module.exports = {
   consumerKey: process.env.MPESA_CONSUMER_KEY,
   consumerSecret: process.env.MPESA_CONSUMER_SECRET,
   passkey: process.env.MPESA_PASSKEY,
-  // We'll use the paybill provided for both BusinessShortCode and PartyB
-  shortcode: "500005",
-  environment: process.env.MPESA_ENV || 'production',
+  shortcode: process.env.MPESA_SHORTCODE,
+  environment: process.env.MPESA_ENV || 'sandbox',
   callbackUrl: process.env.CALLBACK_URL,
+  
+  // API endpoints
   endpoints: {
     auth: function() {
       return this.environment === 'production'
